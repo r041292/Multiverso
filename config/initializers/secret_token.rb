bf85350751f226037e0385440a465954d1672417
Demo <0041292@gmail.com>
2014-10-07 19:50:50 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Multiverso::Application.config.secret_key_base = '33b8627ec9ee289cb00fdc2a72e7e08a59e1be18340686f3f3911bfc98182b1a5f589a550b0caf9d066dc3c59ed916f23e7d44dc943c6218ffdfa5a5c4c700bd'
